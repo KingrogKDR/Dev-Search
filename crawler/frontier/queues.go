62636242249ac043e93c5bc61a19b58e5562773c
KingrogKDR <abhisheksaikia766@gmail.com>
2026-02-26 15:27:32 +0530

    added retry logic to the frontier

--- crawler/frontier/queues.go
+++ crawler/frontier/queues.go
@@ -1,9 +1,19 @@
 package frontier
 
 import (
+	"crypto/rand"
+	"errors"
+	"fmt"
+	"math/big"
+	"net/http"
+	"sync"
 	"sync/atomic"
 	"time"
 )
+
+const VisibilityTimeout = 30 * time.Second
+const MaxRetries = 5
+const BaseBackoff = 5 * time.Second
 
 type JobStatus uint8
 
@@ -31,6 +41,9 @@ var queueSizes = map[uint8]uint64{
 	uint8(P3_LOW):    8192, // recrawls / background
 }
 
+var maxDeadJobs = 1024
+var delayedStoreSize = 1024
+
 type Job struct {
 	ID              string
 	Priority        PriorityStatus
@@ -53,10 +66,20 @@ type DataQ struct {
 }
 
 type PriorityQ struct {
-	queues []*DataQ
+	queues      []*DataQ
+	deadLetterQ []*deadJob
+	delayed     *DelayedStore
 }
 
-// dead letter queue
+type deadJob struct {
+	job *Job
+	err *error
+}
+
+type DelayedStore struct {
+	jobs []*Job
+	mu   sync.Mutex
+}
 
 func NewDataQ(size uint64) *DataQ {
 	if size&(size-1) != 0 {
@@ -75,19 +98,31 @@ func NewDataQ(size uint64) *DataQ {
 	return q
 }
 
+func NewDelayedStore() *DelayedStore {
+	return &DelayedStore{
+		jobs: make([]*Job, 0, delayedStoreSize),
+	}
+}
+
 func NewPriorityQ() *PriorityQ {
 	dataQueues := make([]*DataQ, NumPriorities)
 	for i := range NumPriorities {
 		size := queueSizes[uint8(i)]
 		dataQueues[i] = NewDataQ(size)
 	}
-
-	return &PriorityQ{
-		queues: dataQueues,
+	deadQ := make([]*deadJob, maxDeadJobs)
+	pq := &PriorityQ{
+		queues:      dataQueues,
+		deadLetterQ: deadQ,
+		delayed:     NewDelayedStore(),
 	}
+
+	pq.delayed.Start(pq.Enqueue)
+
+	return pq
 }
 
-func (q *DataQ) Enqueue(job *Job) bool {
+func (q *DataQ) enqueue(job *Job) bool {
 	for {
 		tail := atomic.LoadUint64(&q.tail)
 		cell := &q.buffer[tail&q.mask]
@@ -106,7 +141,7 @@ func (q *DataQ) Enqueue(job *Job) bool {
 	}
 }
 
-func (q *DataQ) Dequeue() (*Job, bool) {
+func (q *DataQ) dequeue() (*Job, bool) {
 	for {
 		head := atomic.LoadUint64(&q.head)
 		cell := &q.buffer[head&q.mask]
@@ -116,7 +151,7 @@ func (q *DataQ) Dequeue() (*Job, bool) {
 		if diff == 0 {
 			if atomic.CompareAndSwapUint64(&q.head, head, head+1) {
 				job := cell.value
-				atomic.StoreUint64(&q.head, head+q.mask+1) // slot is available for reuse after reading
+				atomic.StoreUint64(&cell.seqNo, head+q.mask+1) // slot is available for reuse after reading
 				return job, true
 			}
 		} else if diff < 0 {
@@ -125,15 +160,109 @@ func (q *DataQ) Dequeue() (*Job, bool) {
 	}
 }
 
-func (pq *PriorityQ) Enqueue(job *Job) bool {
-	return pq.queues[job.Priority].Enqueue(job)
+func (ds *DelayedStore) Add(job *Job) {
+	ds.mu.Lock()
+	ds.jobs = append(ds.jobs, job)
+	ds.mu.Unlock()
 }
 
-func (pq *PriorityQ) Dequeue() (*Job, bool) {
+func (ds *DelayedStore) Start(reEnqueue func(*Job) bool) {
+	ticker := time.NewTicker(1 * time.Second)
+
+	go func() {
+		for range ticker.C {
+			now := time.Now()
+
+			ds.mu.Lock()
+			remaining := ds.jobs[:0]
+
+			for _, job := range ds.jobs {
+				if now.After(job.VisibilityUntil) {
+					reEnqueue(job)
+				} else {
+					remaining = append(remaining, job)
+				}
+			}
+
+			ds.jobs = remaining
+			ds.mu.Lock()
+		}
+	}()
+}
+
+func (pq *PriorityQ) Enqueue(job *Job) bool {
+	return pq.queues[job.Priority].enqueue(job)
+}
+
+func (pq *PriorityQ) Pull() (*Job, JobStatus) {
 	for p := range NumPriorities {
-		if job, ok := pq.queues[p].Dequeue(); ok {
-			return job, true
+		if job, ok := pq.queues[p].dequeue(); ok {
+			job.Status = JobInFlight
+			job.VisibilityUntil = time.Now().Add(VisibilityTimeout)
+			return job, JobInFlight
 		}
 	}
-	return nil, false
+	return nil, JobReady
+}
+
+type HTTPError struct {
+	Status int
+	Err    error
+}
+
+func (e *HTTPError) Error() string {
+	return fmt.Sprintf("status %d: %v", e.Status, e.Err)
+}
+
+func isPermanentFailure(err error) bool {
+	var httpErr *HTTPError
+
+	if errors.As(err, &httpErr) {
+		if httpErr.Status == http.StatusNotFound ||
+			httpErr.Status == http.StatusGone {
+			return true
+		}
+	}
+	return false
+}
+
+func (pq *PriorityQ) HandleFailure(job *Job, err *error, score int) {
+
+	if isPermanentFailure(*err) {
+		job.Status = JobDead
+		jobOver := &deadJob{
+			job: job,
+			err: err,
+		}
+		pq.deadLetterQ = append(pq.deadLetterQ, jobOver)
+		return
+	}
+
+	job.RetryCount++
+
+	if job.RetryCount > MaxRetries {
+		job.Status = JobDead
+		jobOver := &deadJob{
+			job: job,
+			err: err,
+		}
+		pq.deadLetterQ = append(pq.deadLetterQ, jobOver)
+		return
+	}
+	delay := BaseBackoff * time.Duration(1<<job.RetryCount)
+	jitter, _ := rand.Int(rand.Reader, big.NewInt(int64(delay/2)))
+	jitterTime := time.Duration(jitter.Int64())
+	delay = delay - delay/4 + jitterTime
+
+	job.VisibilityUntil = time.Now().Add(delay)
+	job.Status = JobReady
+
+	score -= int(job.RetryCount * 10)
+
+	job.Priority = ScoreToPriority(score)
+	if job.Priority < P3_LOW {
+		job.Priority++
+	}
+
+	pq.delayed.Add(job)
 }
--- crawler/worker/fetcher.go
+++ crawler/worker/fetcher.go
@@ -6,12 +6,28 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+
+	"github.com/KingrogKDR/Dev-Search/crawler/frontier"
 )
 
+// update status of the url in queue and turn it hidden from other workers.
+// if worker crashes before the visibility timer, retry count is added in the url metadata and a backoff algorithm is used which reduces the score of this url and pushes it towards lower priority
 // fetch the url's HTML
 // check for content-seen parameters (hash / similarity parameters)
 // store it in S3 if the check for content deduplication is
 // store domain information in domain metadata
+// store the url metadata in url metadata
+
+func ProcessFromQueue(pq *frontier.PriorityQ) error {
+	job, status := pq.Pull()
+	// check for the job.payload = url, its domain metadata and get robots.txt if available, otherwise call the robots.txt fetch func
+	// fetch url
+	// if succeeds
+	return nil
+
+}
+
+func FetchRobots(rawUrl string)
 
 func FetchPageForUrl(rawUrl string, userAgent string, crawler *CrawlerClient) error {
 	parsedUrl, err := url.Parse(rawUrl)
@@ -28,7 +44,6 @@ func FetchPageForUrl(rawUrl string, userAgent string, crawler *CrawlerClient) er
 	fmt.Println("User-Agent:", userAgent)
 
 	ctx := context.Background()
-
 	robots, err := GetRobotsForDomain(ctx, scheme, domain, crawler)
 	if err != nil {
 		log.Printf("Error getting robots for the domain: %v\n", err)
